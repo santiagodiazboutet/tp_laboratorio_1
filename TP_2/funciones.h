e3f2c27ff93760150bf9fa8f9980dfbc9aaaf542
santiagodiazboutet <santiagodiazboutet@gmail.com>
2018-05-11 22:49:51 -0300

    Add files via upload

--- TP_2/funciones.h
+++ TP_2/funciones.h
@@ -11,16 +11,18 @@ typedef struct {
 }ePersona;
 
 /**
- * Obtiene el indice que coincide con el dni pasado por parametro.
+ * Inicia la variable estado de todos los miembros del array en LIBRE
  * @param lista el array se pasa como parametro.
- * @param dni el dni a ser buscado en el array.
- * @return el indice en donde se encuentra el elemento que coincide con el parametro dni
+ * @param tamaño maximo del array
+ * @return codigo de control de error
  */
-//int buscarPorDni(EPersona lista[], int dni);
-
-
 int ePersona_init(ePersona[],int limite);
-
+/**
+ * Permite al usuario ingresar datos al array
+ * @param lista el array se pasa como parametro.
+ * @param tamaño maximo del array
+ * @return codigo de control de error
+ */
 int ePersona_alta(ePersona[],int limite);
 /**
  * Obtiene el primer indice libre del array.
@@ -29,8 +31,22 @@ int ePersona_alta(ePersona[],int limite);
  * @return el primer indice disponible
  */
 int ePersona_buscarLugarLibre(ePersona[],int);
+/**
+ * Imprime los valores de una estructura
+ * @param parametro a imprimir
+ */
 void ePersona_mostrarUno(ePersona);
+/**
+ * Imprime todos los miembros del array de la estructura
+ * @param lista el array se pasa como parametro.
+ * @param tamaño maximo del array
+ */
 void ePersona_mostrarListado(ePersona[],int);
+/**
+ * Ordena alfabeticamente el array de una estructura
+ * @param lista el array se pasa como parametro.
+ * @param tamaño maximo del array
+ */
 void ePersona_ordenar(ePersona[],int);
 
 /**
@@ -48,6 +64,12 @@ void ePersona_baja(ePersona listado[] ,int limite);
  * @return el indice en donde se encuentra el elemento que coincide con el parametro dni
  */
 int ePersona_buscarPordni(ePersona listado[] ,int limite, int dni);
+/**
+ * Imprime grafico de las edades que tine la estructura
+ * @param lista el array se pasa como parametro.
+ * @param tamaño maximo del array
+ * @return codigo de control de error
+ */
 void ePersona_grafico(ePersona lista[],int limite);
 
 

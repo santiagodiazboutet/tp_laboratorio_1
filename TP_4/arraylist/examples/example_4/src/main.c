e422bcc277cf9673df26021ecd83b5a0e40b2175
santiagodiazboutet <santiagodiazboutet@gmail.com>
2018-06-29 10:34:35 -0300

    Add files via upload

--- TP_4/arraylist/examples/example_4/src/main.c
+++ TP_4/arraylist/examples/example_4/src/main.c
@@ -28,23 +28,23 @@
 
 int main(void)
 {
-    // startTesting(1);
-    //startTesting(2);
-    // startTesting(3);
-    // startTesting(4);
-    // startTesting(5);
-    // startTesting(6);
-    // startTesting(7);
-    //startTesting(8);
-    //startTesting(9);
-    //startTesting(10);
-    // startTesting(11);
-    // startTesting(12);
-    // startTesting(13);
+    startTesting(1);
+    startTesting(2);
+     startTesting(3);
+     startTesting(4);
+     startTesting(5);
+     startTesting(6);
+     startTesting(7);
+    startTesting(8);
+    startTesting(9);
+    startTesting(10);
+    startTesting(11);
+    startTesting(12);
+     startTesting(13);
      startTesting(14);
-    // startTesting(15);
-    // startTesting(16);
-    // startTesting(17);*/
+     startTesting(15);
+     startTesting(16);
+     startTesting(17);
 
     return 0;
 }
